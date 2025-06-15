7ac64cf82ccdb6ba6873037836bb7ce948dace12
MichaelP591 <mhernandez0520@icloud.com>
2025-06-15 14:42:23 -0400

    idk

--- Assets/Scripts/BaseGame/Pot.cs
+++ Assets/Scripts/BaseGame/Pot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using BaseGame;
 
-public class Pot : MonoBehaviour
+public class Pot 
 {
     public int Amount;
     public HashSet<Player> EligiblePlayers;
